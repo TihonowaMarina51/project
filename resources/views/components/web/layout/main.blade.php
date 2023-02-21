4063561439cd4025c75d506e6a5b6244e23bda2a
admin <tihonovamira301@gmail.com>
2023-02-21 12:03:51 +0300

    Added header

---
+++ resources/views/components/web/layout/main.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$title??''}}</title>
+    @vite(['resources/css/app.css'])
+</head>
+<body>
+<x-web.block.header></x-web.block.header>
+<div class="container-lg p-2">
+
+</div>
+</body>
+</html>
